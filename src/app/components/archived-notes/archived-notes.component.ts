346e3d093d722fe230d00abfd455b0c55a905e9d
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 12:50:10 +0000

    Add archived notes component

---
+++ src/app/components/archived-notes/archived-notes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archived-notes',
+  imports: [],
+  templateUrl: './archived-notes.component.html',
+  styleUrl: './archived-notes.component.scss'
+})
+export class ArchivedNotesComponent {
+
+}
