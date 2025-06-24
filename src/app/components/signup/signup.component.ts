b2e3e46a742f6b405f86c7df591a85ff52e3f904
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 17:05:09 +0000

    add logic to handle form in the the sign up component

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './signup.component.html',
+  styleUrls: ['../../shared/auth.component.scss'],
+})
+export class SignupComponent {
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signupForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.signupForm.valid) {
+      // Save user to localStorage or call backend API
+      localStorage.setItem('user', JSON.stringify(this.signupForm.value));
+      this.router.navigate(['/notes']);
+    }
+  }
+}
