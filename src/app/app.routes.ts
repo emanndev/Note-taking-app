9b8080bfededd855bcea834fcb37c74fc55b1354
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 11:52:53 +0000

    Add html to app template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'notes',
+    loadComponent: () =>
+      import('./components/notes-dashboard/notes-dashboard.component'),
+  },
+  {
+    path: 'archived',
+    loadComponent: () =>
+      import('./components/archived-notes/archived-notes.component'),
+  },
+  {
+    path: 'notes/:id',
+    loadComponent: () =>
+      import('./components/note-details/note-details.component'),
+  },
+  {
+    path: 'create',
+    loadComponent: () =>
+      import('./components/note-create/note-create.component'),
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./components/settings/settings.component'),
+  },
+  { path: '', redirectTo: '/notes', pathMatch: 'full' },
+];
