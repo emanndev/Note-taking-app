6be5ab659f025ace8dc6c6ecce5098f68524c199
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 11:49:45 +0000

    Add logic to handle crud operations in notes

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { Note } from '../models/note.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NoteService {
+  private notes: Note[] = [];
 
-  constructor() { }
+  createNote(note: Note) {
+    note.id = Date.now().toString();
+    note.createdAt = new Date();
+    this.notes.push(note);
+    return note;
+  }
+
+  getNotes() {
+    return this.notes.filter((note) => !note.isArchived);
+  }
+
+  getArchivedNotes() {
+    return this.notes.filter((note) => note.isArchived);
+  }
+
+  getNoteById(id: string) {
+    return this.notes.find((note) => note.id === id);
+  }
+
+  updateNote(updatedNote: Note) {
+    const index = this.notes.findIndex((note) => note.id === updatedNote.id);
+    if (index !== -1) this.notes[index] = updatedNote;
+  }
+
+  deleteNote(id: string) {
+    this.notes = this.notes.filter((note) => note.id !== id);
+  }
+
+  archiveNote(id: string, archive: boolean) {
+    const note = this.getNoteById(id);
+    if (note) note.isArchived = archive;
+  }
+
+  filterNotes(query: string) {
+    return this.notes.filter(
+      (note) =>
+        note.title.toLowerCase().includes(query.toLowerCase()) ||
+        note.content.toLowerCase().includes(query.toLowerCase()) ||
+        note.tags.some((tag) => tag.toLowerCase().includes(query.toLowerCase()))
+    );
+  }
 }
