997bf851c4a39ca0a099fc17691a4f7413688325
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 20:48:02 +0000

    Generate forgot-password component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { NotesDashboardComponent } from './components/notes-dashboard/notes-dashboard.component';
 import { ArchivedNotesComponent } from './components/archived-notes/archived-notes.component';
 import { NoteDetailsComponent } from './components/note-details/note-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/notes', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
   { path: 'notes', component: NotesDashboardComponent },
+
   { path: 'notes/:id', component: NotesDashboardComponent },
   { path: 'archived', component: NotesDashboardComponent },
   { path: 'create', component: NotesDashboardComponent },
