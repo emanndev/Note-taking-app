6d869bb5d737656e2a281ac94a7a256934f7555b
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 11:54:23 +0000

    Add html to the notes dashboard

---
+++ src/app/components/notes-dashboard/notes-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-dashboard',
+  imports: [],
+  templateUrl: './notes-dashboard.component.html',
+  styleUrl: './notes-dashboard.component.scss'
+})
+export class NotesDashboardComponent {
+
+}
