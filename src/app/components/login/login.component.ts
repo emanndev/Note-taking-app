c8baee9bc5732cb9ed88d64fcc8a2e66ab8a7028
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 17:01:06 +0000

    Add logic to handle form in login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./auth.component.scss'],
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    const savedUser = JSON.parse(localStorage.getItem('user') || '{}');
+    const { email, password } = this.loginForm.value;
+
+    if (email === savedUser.email && password === savedUser.password) {
+      this.router.navigate(['/notes']);
+    } else {
+      alert('Invalid credentials');
+    }
+  }
 }
