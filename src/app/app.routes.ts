50ec9d220b75009b1df365eee72fff241d31a634
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 20:55:23 +0000

    Add forgot-passowrd route to the routes page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,15 @@ import { ArchivedNotesComponent } from './components/archived-notes/archived-not
 import { NoteDetailsComponent } from './components/note-details/note-details.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
-
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { SettingsComponent } from './components/settings/settings.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/notes', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'notes', component: NotesDashboardComponent },
-
   { path: 'notes/:id', component: NotesDashboardComponent },
   { path: 'archived', component: NotesDashboardComponent },
   { path: 'create', component: NotesDashboardComponent },
