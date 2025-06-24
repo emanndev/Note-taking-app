3561c477463f5cfaaefe6913f9d7b6c1ed5013e1
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-24 20:49:18 +0000

    Add logic for forgot-password form

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.scss'
+  styleUrls: ['../../shared/auth.component.scss'],
 })
 export class ForgotPasswordComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      alert(`Password reset link sent to ${this.form.value.email}`);
+    }
+  }
 }
