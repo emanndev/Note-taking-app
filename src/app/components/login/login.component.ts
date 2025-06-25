828098781f6a442522e51726b2f394930e366d86
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-25 10:02:49 +0000

    Add router link in login component to handle routing to the signup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,13 +5,13 @@ import {
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrls: ['../../shared/auth.component.scss'],
 })
