8bf4fc0d7fb125d03a40dbd88ce32b119138a423
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-26 10:03:12 +0000

    Set up an authentication service to handle user login and logout

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private tokenKey = 'authToken';
+  constructor(private router: Router) {}
 
-  constructor() { }
+  //saving token in localstorage
+  login(): void {
+    localStorage.setItem(this.tokenKey, 'myFakeToken');
+  }
+
+  //removing token from localstorage
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.router.navigate(['/login']);
+  }
+
+  //checking if token is present to authenticate user
+  isAuthenticated(): boolean {
+    const token = localStorage.getItem(this.tokenKey);
+    return token !== null;
+  }
 }
