cde62da4a4d16ae7e1ad6b0124f7263b545f7759
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-06-26 10:16:18 +0000

    Implement route guard logic to handle user access to the application

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  // check if user is authenticated and allow access
+  if (authService.isAuthenticated()) {
+    return true;
+  } else {
+    // if not authenticated, redirect to login
+    router.navigate(['/login']);
+    return false;
+  }
 };
